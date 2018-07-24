eeb77d7ec81b2a758550872094e9dc87a809cd23
James <wilfongjt@gmail.com>
2018-07-24 05:28:19 -0400

    minor change comments

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+// import Dotenv from 'dotenv'
 import Vue from 'vue'
 // import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
+// require('dotenv').config()
+// import Dotenv from 'dotenv-webpack'
+// Dotenv.config()
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Vue.use(BootstrapVue)
+// Dotenv.config()
 
 /* eslint-disable no-new */
 new Vue({
